026514ef7051cbedcff88f9d9b6f0fc93355649e
David Cristiano <cristianodd3097@gmail.com>
2025-07-30 16:59:46 -0300

    fix: Corrijindo caminhos das imagens

--- components/PastSumAboutPokemon/funcSumAboutPokemon.js
+++ components/PastSumAboutPokemon/funcSumAboutPokemon.js
@@ -8,7 +8,7 @@ import {
 } from "../PastUtilitiesObjectAndFunction/funcUtilitiesStyleMapping.js";
 
 export const funcSumAboutPokemon = async (inserirInTag) => {
-  const basePath = "../../image/iconsStates/"
+  const basePath = new URL("../../image/iconsStates/", import.meta.url).pathname
   const params = new URLSearchParams(window.location.search);
 
   const pokemon = {
